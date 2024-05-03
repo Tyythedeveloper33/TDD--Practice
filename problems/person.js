4c442d43c6bb990ee1d386edaa4323c98f04b4e0
tylerwebster <webstertyler81@yahoo.com>
2024-05-02 20:00:39 -0400

    11 passing tdd test

--- problems/person.js
+++ problems/person.js
@@ -1,5 +1,14 @@
 class Person {
-  // Your code here 
+  constructor(name, age ){
+    this.name = name;
+    this.age = age;
+  }
+  sayHello(){
+    return `Hello my name is ${this.name}`
+  }
+  visit(otherPerson){
+    return `${this.name} visited ${otherPerson}`
+  }
 }
 
 module.exports = Person;
--- test/person-spec.js
+++ test/person-spec.js
@@ -1,1 +1,37 @@
-// Your code here 
+const chai = require('chai');
+const expect = chai.expect;
+const Person = require('../problems/person')
+
+describe('Person class', () => {
+    let person;
+    beforeEach(() => {
+    person = new Person('Tyler')
+    })
+
+    it('should create an instance of person class', () => {
+        expect(person).to.exist
+    })
+
+    it('should have properties of "name"', ()=>{
+        expect(person).to.have.property('name'  )
+    })
+    it('should have properties of "age"', ()=>{
+        expect(person).to.have.property('age')
+    })
+    it('should have method sayHello', ()=>{
+
+        expect(person.sayHello).to.exist
+    })
+    it('sayHello should return a greeting ' ,()=>{
+        let message = person.sayHello()
+        expect(message).to.equal(`Hello my name is ${person.name}`)
+    })
+    it('should have method "visit"', ()=>{
+        expect(person.visit).to.exist
+    })
+    it('should return visitation"', ()=>{
+
+        let visitation = person.visit("Joshua")
+        expect(visitation).to.equal('Tyler visited Joshua')
+    })
+})
